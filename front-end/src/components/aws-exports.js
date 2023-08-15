14ff6f4fb65f9c9dcbc1ca43c13114e656da99b9
BorisLaboratory <greatecboris@gmail.com>
2023-08-15 19:45:18 -0400

    Login Component - first commit

---
+++ front-end/src/components/aws-exports.js
@@ -0,0 +1,5 @@
+export const awsExports = {
+  REGION: "us-east-1", //add_your_region_here,
+  USER_POOL_ID: "", //add_your_user_pool_id_here from cognito AWS console page
+  USER_POOL_APP_CLIENT_ID: "", // from cognito AWS console>App integration page
+};
