1c13a6800d10681a3a5a50aae066c295d996b1dc
collinsonine <charlznjoagwu@gmail.com>
2023-09-02 04:10:23 +0100

    new components added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Landingpage from './components/Landingpage';
+import SuccessPopup from './components/SuccessPopup';
+import UserInformationScreen from './components/UserInformationScreen';
 
 function App() {
   return (
     <div className="App">
-      <Landingpage/>
+      <Landingpage/><p/>
+      <UserInformationScreen/><p/>
+      <SuccessPopup/>
     </div>
   );
 }
