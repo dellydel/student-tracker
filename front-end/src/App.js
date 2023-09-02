69d2b89021f5e239834028c2cd2da994d4778bd3
AzukaUgbekile <112770826+AzukaUgbekile@users.noreply.github.com>
2023-09-02 12:02:21 +0100

    Dynamodb Table name change and Appbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,17 @@
-import './App.css';
-import { Typography } from '@mui/material';
-import RegisterCourse from './components/RegisterCourse';
+import "./App.css";
+import { Typography } from "@mui/material";
+import RegisterCourse from "./components/RegisterCourse";
+import Navigation from "./components/Navigation";
 
 const App = () => {
   return (
     <div className="App">
-    <Typography gutterBottom variant='h4'>
-      Course Registration Form
-    </Typography>
-    <RegisterCourse />
-  </div>
+      <Typography gutterBottom variant="h4">
+        Course Registration Form
+      </Typography>
+      <RegisterCourse />
+      <Navigation />
+    </div>
   );
 };
 
---
+++ front-end/src/components/Navigation.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { AppBar, Toolbar, Typography, Button, Box, Stack } from "@mui/material";
+//import stack from "@mui/material";
+
+function Navigation() {
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1, backgroundColor: "purple" }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography
+              variant="h2"
+              component="div"
+              sx={{ flexGrow: 1 }}
+            ></Typography>
+            <Stack direction="row" spacing={2}>
+              <Button color="inherit">Home</Button>
+              <Button color="inherit">View Courses</Button>
+            </Stack>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </div>
+  );
+}
+
+export default Navigation;
