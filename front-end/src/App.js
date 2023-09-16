90d21f40891d321a00adcbee31589726a16d4f52
Delvin <dlvhall@gmail.com>
2023-09-16 10:15:19 -0500

    updating home

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,40 +1,38 @@
-import "./App.css";
-import RegisterCourse from "./components/RegisterCourse";
-import Login from "./components/Login";
-import CourseList from "./components/CourseList";
-import CourseDetail from "./components/CourseDetail";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import LandingPage from "./components/LandingPage";
-import UserInformationScreen from "./components/UserInformationScreen";
-import "./App.css";
-import Home from "./pages/Home";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import "./App.css";
+import "./App.css";
+import CourseDetail from "./components/CourseDetail";
+import CourseList from "./components/CourseList";
+import LandingPage from "./components/LandingPage";
+import Layout from "./components/Layout";
+import RegisterCourse from "./components/RegisterCourse";
+import UserInformationScreen from "./components/UserInformationScreen";
 
 const theme = createTheme({
-  typography: {
-    allVariants: {
-      fontFamily: "Rubik",
-    },
-  },
+	typography: {
+		allVariants: {
+			fontFamily: "Rubik",
+		},
+	},
 });
 
 const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<LandingPage />} />
-            <Route path="/user" element={<UserInformationScreen />} />
-            <Route path="/courses" element={<CourseList />} />
-            <Route path="/course-details" element={<CourseDetail />} />
-            <Route path="/view-courses" element={<CourseList />} />
-            <Route path="/register" element={<RegisterCourse />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Layout />}>
+						<Route index element={<LandingPage />} />
+						<Route path="/user" element={<UserInformationScreen />} />
+						<Route path="/courses" element={<CourseList />} />
+						<Route path="/course-details" element={<CourseDetail />} />
+						<Route path="/view-courses" element={<CourseList />} />
+						<Route path="/register" element={<RegisterCourse />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</ThemeProvider>
+	);
 };
 export default App;
