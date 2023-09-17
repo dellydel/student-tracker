b660e3a417901c66206399648d4347ca2cfb6624
Delvin <dlvhall@gmail.com>
2023-09-17 12:19:23 -0500

    refactor of layout

--- front-end/src/components/Layout.js
+++ front-end/src/components/Layout.js
@@ -1,11 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Navigation from "./Navigation";
+
 const Layout = () => {
   return (
     <>
-      <nav>
-        <Navigation />
-      </nav>
+      <Navigation />
       <Outlet />
     </>
   );
