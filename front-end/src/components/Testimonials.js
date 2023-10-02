3b88275ca0b7025ee8cb2cb7d2ca04b66e66efcb
BorisLaboratory <greatecboris@gmail.com>
2023-10-02 17:47:30 -0400

    Testimonials section added

--- front-end/src/components/Testimonials.js
+++ front-end/src/components/Testimonials.js
@@ -1,7 +1,46 @@
 import React from "react";
+import Avatar from "@mui/material/Avatar";
+import testimonialData from "../test-data/testimonials";
 
 const Testimonials = () => {
-	return <div>Testimonials [Placeholder]</div>;
+	return (
+		<>
+			<h3>Testimonials</h3>
+			<div
+				style={{
+					display: "flex",
+					flexDirection: "row",
+					justifyItems: "center",
+					justifyContent: "space-between",
+					gap: "50px",
+					marginBottom: "20px",
+				}}
+			>
+				{testimonialData.map((testimonial) => {
+					return (
+						<div>
+							<span>
+								<Avatar src={testimonial.avatarUrl} atl="student"></Avatar>
+								<br />
+								<span
+									style={{ fontStyle: "italic" }}
+								>{`"${testimonial.testimonial}"`}</span>
+								<p
+									style={{
+										textAlign: "right",
+										marginRight: 50,
+										color: "black",
+										fontWeight: 900,
+									}}
+								>
+									{testimonial.FirstName}&nbsp;{testimonial.LastInitial}.
+								</p>
+							</span>
+						</div>
+					);
+				})}
+			</div>
+		</>
+	);
 };
-
 export default Testimonials;
---
+++ front-end/src/test-data/testimonials.js
@@ -0,0 +1,25 @@
+const testimonialData = [
+	{
+		avatarUrl: "https://i.pravatar.cc/150?img=59",
+		testimonial:
+			"The very Bootcamp every newbie needs to attend. It was a very nice experience learning React and Nodejs. AWS Cloudformation was a Bomb ! ..",
+		FirstName: "Chris",
+		LastInitial: "E",
+	},
+	{
+		avatarUrl: "https://i.pravatar.cc/150?img=57",
+		testimonial:
+			"This was a great mile stone attained. I recommend Next Byte trainings for any one Looking for where to learn Advanced javascript and Front End Development..",
+		FirstName: "Henry",
+		LastInitial: "I",
+	},
+	{
+		avatarUrl: "https://i.pravatar.cc/150?img=44",
+		testimonial:
+			"This is the best thing that has ever hapened to me in a decade..",
+		FirstName: "Chidinma",
+		LastInitial: "I",
+	},
+];
+
+export default testimonialData;
