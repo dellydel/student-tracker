23270fd4a95d6c6a7b5f05d7e731ae53f333641f
collinsonine <charlznjoagwu@gmail.com>
2023-10-06 14:36:06 +0100

    Resolved Conflicts

--- front-end/src/components/Navigation.js
+++ front-end/src/components/Navigation.js
@@ -1,4 +1,4 @@
-import React, { useState, RouterLink } from "react";
+import React from "react";
 import { AppBar, Toolbar, Link, Box, Stack } from "@mui/material";
 import Avatar from "@mui/material/Avatar";
 
@@ -9,52 +9,81 @@ const pages = [
 
 function Navigation({ isLogin = true }) {
 	return (
-		<div>
-			<Box sx={{ flexGrow: 1 }}>
-				<AppBar position="fixed" color="transparent" elevation={0}>
-					<Toolbar>
-						<Stack
-							direction="row"
-							spacing={8}
+		<AppBar
+			position="fixed"
+			sx={{ background: "transparent", boxShadow: "none" }}
+		>
+			<Toolbar>
+				<Box
+					sx={{
+						pl: 5,
+						flexGrow: 1,
+						display: "flex",
+						justifyContent: "flex-start",
+					}}
+				>
+					<Link
+						href={"/"}
+						sx={{ m: 1, p: 1, color: "white", textDecoration: "none" }}
+					>
+						<Box
+							component="img"
+							src={"/logo-white.png"}
+							alt={"NextByte Logo"}
+							sx={{ height: "50px", width: "auto" }}
+						/>
+					</Link>
+				</Box>
+				<Box
+					sx={{
+						flexGrow: 1,
+						display: "flex",
+						justifyContent: "flex-end",
+					}}
+				>
+					{pages.map((page) => (
+						<Link
+							key={page.name}
+							href={page.link}
 							sx={{
-								flexGrow: 1,
-								display: "flex",
-								justifyContent: "flex-end",
+								m: 1,
+								p: 2,
+								color: "white",
+								textDecoration: "none",
+								fontWeight: "bold",
+								fontSize: "1.2rem",
 							}}
 						>
-							<Box>
-								{pages.map((page) => (
-									<Link
-										key={page}
-										href={page.link}
-										sx={{ m: 1, p: 1, color: "white", textDecoration: "none" }}
-									>
-										{page.name}
-									</Link>
-								))}
-							</Box>
-							<Box>
-								{isLogin ? (
-									<Stack direction="row">
-										<Avatar sx={{ width: 25, height: 25, mb: 2, mr: 1 }}>
-											ID
-										</Avatar>
-										<Box sx={{ color: "white" }}>USER NAME</Box>
-									</Stack>
-								) : (
-									<Link
-										href={"/Login"}
-										sx={{ m: 1, p: 1, color: "white", textDecoration: "none" }}
-									>
-										LOGIN
-									</Link>
-								)}
-							</Box>
-						</Stack>
-					</Toolbar>
-				</AppBar>
-			</Box>
-		</div>
+							{page.name}
+						</Link>
+					))}
+					<Box
+						sx={{
+							m: 1,
+							p: 2,
+							color: "white",
+							textDecoration: "none",
+							fontWeight: "bold",
+							fontSize: "1.2rem",
+						}}
+					>
+						{isLogin ? (
+							<Stack direction="row">
+								<Avatar sx={{ width: 25, height: 25, mb: 2, mr: 1 }}>ID</Avatar>
+								<Box sx={{ color: "white" }}>USER NAME</Box>
+							</Stack>
+						) : (
+							<Link
+								href={"/Login"}
+								sx={{ m: 1, p: 1, color: "white", textDecoration: "none" }}
+							>
+								LOGIN
+							</Link>
+						)}
+					</Box>
+				</Box>
+			</Toolbar>
+		</AppBar>
 	);
 }
 
