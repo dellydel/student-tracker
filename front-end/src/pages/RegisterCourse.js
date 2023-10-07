5ea8ce4ec192b0183ac76b150d2caed4eade817f
AzukaUgbekile <112770826+AzukaUgbekile@users.noreply.github.com>
2023-10-07 16:48:23 +0100

    Aptfield removed

--- front-end/src/pages/RegisterCourse.js
+++ front-end/src/pages/RegisterCourse.js
@@ -17,7 +17,6 @@ const RegisterCourse = () => {
 	const [phoneNumber, setPhoneNumber] = useState("");
 	const [email, setEmail] = useState("");
 	const [street, setStreet] = useState("");
-	const [apartmentNo, setApartmentNo] = useState("");
 	const [city, setCity] = useState("");
 	const [state, setState] = useState("");
 	const [zip, setZip] = useState("");
@@ -52,7 +51,6 @@ const RegisterCourse = () => {
 				phoneNumber,
 				email,
 				street,
-				apartmentNo,
 				city,
 				state,
 				zip,
@@ -167,18 +165,6 @@ const RegisterCourse = () => {
 									fullWidth
 									required
 									onChange={(event) => setStreet(event.target.value)}
-								/>
-							</Grid>
-							<Grid xs={12} sm={3} item>
-								<TextField
-									type="number"
-									label="Apt#"
-									name="apartmentNo"
-									placeholder="Enter house number"
-									variant="outlined"
-									fullWidth
-									required
-									onChange={(event) => setApartmentNo(event.target.value)}
 								/>
 							</Grid>
 							<Grid xs={12} sm={4} item>
