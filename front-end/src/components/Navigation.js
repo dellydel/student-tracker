279a0d6b1c283673b2db55164866d057750d635a
collinsonine <charlznjoagwu@gmail.com>
2023-10-07 16:55:26 +0100

    Navigation Updated

--- front-end/src/components/Navigation.js
+++ front-end/src/components/Navigation.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { AppBar, Toolbar, Link, Box } from "@mui/material";
+import { AppBar, Toolbar, Link, Box, Stack } from "@mui/material";
+import Avatar from "@mui/material/Avatar";
 
 const pages = [
 	{ name: "HOME", link: "/" },
 	{ name: "COURSES", link: "/courses" },
 ];
-
-function Navigation() {
+function Navigation({ isLogin = true }) {
 	return (
 		<AppBar
 			position="fixed"
@@ -56,10 +56,33 @@ function Navigation() {
 							{page.name}
 						</Link>
 					))}
+					<Box
+						sx={{
+							m: 1,
+							p: 2,
+							color: "white",
+							textDecoration: "none",
+							fontWeight: "bold",
+							fontSize: "1.2rem",
+						}}
+					>
+						{isLogin ? (
+							<Stack direction="row">
+								<Avatar sx={{ width: 25, height: 25, mb: 2, mr: 1 }}>ID</Avatar>
+								<Box sx={{ color: "white" }}>USER NAME</Box>
+							</Stack>
+						) : (
+							<Link
+								href={"/Login"}
+								sx={{ m: 1, p: 1, color: "white", textDecoration: "none" }}
+							>
+								LOGIN
+							</Link>
+						)}
+					</Box>
 				</Box>
 			</Toolbar>
 		</AppBar>
 	);
 }
-
 export default Navigation;
