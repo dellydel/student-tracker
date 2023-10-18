4d621cb40556ff77976855da06a4311be5c850f8
Delvin <dlvhall@gmail.com>
2023-10-18 10:53:50 -0500

    comments

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Box } from "@mui/material";
 import Features from "../components/Features";
 import MainHero from "../components/MainHero";
-import Pricing from "../components/Pricing";
+//import Pricing from "../components/Pricing";
 import SectionDivider from "../components/SectionDivider";
 import Testimonials from "../components/Testimonials";
 
