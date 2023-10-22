0b94fb84d98bc1aa8e25f7b3212380dad3d279ef
AzukaUgbekile <112770826+AzukaUgbekile@users.noreply.github.com>
2023-10-22 22:14:20 +0100

    Added a new field

--- front-end/src/pages/RegisterCourse.js
+++ front-end/src/pages/RegisterCourse.js
@@ -22,6 +22,7 @@ const RegisterCourse = () => {
 	const [setCity] = useState("");
 	const [setState] = useState("");
 	const [setZip] = useState("");
+	const [setCountry] = useState("");
 	const [setDateOfBirth] = useState("");
 	const [feedBackText, setFeedBackText] = useState("");
 	const [isLoading] = useState(false);
@@ -58,6 +59,7 @@ const RegisterCourse = () => {
 	// 			city,
 	// 			state,
 	// 			zip,
+
 	// 			dateOfBirth,
 	// 		},
 	// 		TableName: "course-reg-dynamodb-2023",
@@ -205,6 +207,17 @@ const RegisterCourse = () => {
 									fullWidth
 									required
 									onChange={(event) => setZip(event.target.value)}
+								/>
+							</Grid>
+							<Grid type="number" xs={12} item>
+								<TextField
+									label="Country"
+									name="Country"
+									placeholder="Enter Your Country"
+									variant="outlined"
+									fullWidth
+									required
+									onChange={(event) => setCountry(event.target.value)}
 								/>
 							</Grid>
 							<Grid xs={12} item>
