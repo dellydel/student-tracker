bdab0cec96714fe7a2a23344e9cbf4fcc01fe805
Delvin <dlvhall@gmail.com>
2023-10-26 18:41:22 -0500

    infra updates

---
+++ back-end/courses.js
@@ -0,0 +1,25 @@
+const AWS = require("aws-sdk");
+
+exports.handler = async (event) => {
+  console.log(event, "event");
+
+  const docClient = new AWS.DynamoDB.DocumentClient();
+
+  const params = {
+    TableName: "next-byte-Courses-development",
+  };
+
+  let body;
+  let statusCode = 200;
+
+  try {
+    body = await docClient.scan(params).promise();
+  } catch (err) {
+    statusCode = err.statusCode;
+    body = err.message;
+  }
+  return {
+    statusCode,
+    body,
+  };
+};
