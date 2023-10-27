132291d1c181f77e339558da6e7f5335ecf162c3
Delvin <dlvhall@gmail.com>
2023-10-26 21:30:12 -0500

    uninstall lib

--- front-end/src/components/Features.js
+++ front-end/src/components/Features.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { useAutoAnimate } from "@formkit/auto-animate/react";
 import { Box } from "@mui/material";
 import { ourTrainingContent, ourPricingContent } from "../data/featureContent";
 import Feature from "./Feature";
 
 const Features = () => {
-	const [parent, enableAnimations] = useAutoAnimate();
-
 	return (
-		<Box sx={{ mb: 10 }} ref={parent}>
+		<Box sx={{ mb: 10 }}>
 			<Feature textPosition="left" content={ourTrainingContent} />
 			<Feature textPosition="right" content={ourPricingContent} />
 		</Box>
