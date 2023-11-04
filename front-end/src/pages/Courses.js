24852ee12c3c21e59e03bf196da835d31e60b53e
Delvin <dlvhall@gmail.com>
2023-11-04 10:05:36 -0500

    removed unnecessary import

--- front-end/src/pages/Courses.js
+++ front-end/src/pages/Courses.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import axios from "axios";
 import Course from "../components/Course";
 
