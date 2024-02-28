9c86b8ba3802f5755e093fd165e8cb7c05377411
Delvin <dlvhall@gmail.com>
2024-02-27 19:11:05 -0600

    payment complete fix

--- front-end/src/app/payment-complete/page.js
+++ front-end/src/app/payment-complete/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from "react";
 import { Box, Typography, Button } from "@mui/material";
 import { useRouter, useSearchParams } from "next/navigation";
