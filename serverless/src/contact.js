5cd9e91bd80d47470df7a9779dfa3d67be1f2fbb
Delvin <dlvhall@gmail.com>
2024-05-01 16:34:49 -0500

    push to sns when contact requested

--- serverless/src/contact.js
+++ serverless/src/contact.js
@@ -1,14 +1,17 @@
 import httpResponse from "./http_response.js";
+import { SNSClient } from "@aws-sdk/client-sns";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 import { randomUUID } from "crypto";
 
 const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
+const snsClient = new SNSClient({});
 
 export const processContactRequest = async (name, email, message) => {
   const command = generateCommand(name, email, message);
   await docClient.send(command);
+  await sendContactEmailNotification();
   return httpResponse(
     200,
     JSON.stringify({ message: "Success! Your message has been sent." })
@@ -25,5 +28,12 @@ const generateCommand = (name, email, message) => {
       message: message,
       timestamp: Date.now(),
     },
+  });
+};
+
+const sendContactEmailNotification = async () => {
+  await snsClient.publish({
+    TopicArn: process.env.CONTACT_TOPIC,
+    Message: "A new contact / more informtion request has been submitted",
   });
 };
